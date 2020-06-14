8644cbc65b7526160cff46ccdc07b78ceea1a327
zhuanghaixin <649308293@qq.com>
2020-06-14 12:24:18 +0800

    feat: React组件 代码结构优化

---
+++ my-app/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import classes from './Cockpit.css'
+const cockpit=(props)=>{
+    let assignedClasses=[]
+    let btnClass = ''
+    if(props.showPersons){
+        btnClass = classes.Red
+    }
+
+    if(props.persons.length<=2){
+        assignedClasses.push(classes.red)  //classes =['red']
+    }
+    if(props.persons.length<=1){
+        assignedClasses.push(classes.bold)  //classes =['red bold']
+    }
+    return (
+        <div className={classes.Cockpit}>
+            <h1>react</h1>
+            <p className={assignedClasses.join(' ')}>This is really working</p>
+            <button onClick={props.clicked} className={btnClass}>Toggle Person</button>
+            {/*<button onClick={this.switchNameHandler.bind(this, 'Hisen')}>Switch Name第一种方法传参数</button>*/}
+            {/*<button onClick={() => this.switchNameHandler('xxx')}>Switch Name第二种方法传参数</button>*/}
+        </div>
+    )
+}
+export default cockpit
--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -2,10 +2,6 @@ import React from 'react';
 import classes from './Person.module.css'
 
 const person = (props) => {
-    const rnd=Math.random()
-    if(rnd>0.7){
-        throw new Error('Something went wrong')
-    }
     return (
 
             <div className={classes.Person}>
