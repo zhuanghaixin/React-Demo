658d949354b569f2c109ed0ff89bf312432d3464
zhuanghaixin <649308293@qq.com>
2020-06-16 20:33:02 +0800

    feat: React组件 HOC高阶组件

--- my-app/src/components/Persons/Person/Person.js
+++ my-app/src/components/Persons/Person/Person.js
@@ -1,19 +1,25 @@
 import React,{Component} from 'react';
 import classes from './Person.module.css'
-
+import Aux from '../../../hoc/Aux'
 class Person extends Component{
     render(){
         console.log('[Person.js] rendering')
         return (
-
-            <div className={classes.Person}>
+            <Aux>
                 <p
+                    key="i1"
                     onClick={this.props.click}
-                >I'm {this.props.name}, I'm {this.props.age} years old</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} defaultValue={this.props.name}/>
-            </div>
-        )
+                >I'm {this.props.name}, I'm {this.props.age} years old</p>,
+                <p
+                    key="i2"
+                >{this.props.children}</p>,
+                <input
+                    key="i3"
+                    type="text"
+                    onChange={this.props.changed}
+                    defaultValue={this.props.name}/>
+            </Aux>
+    )
     }
 
 }
--- my-app/src/components/Persons/Persons.js
+++ my-app/src/components/Persons/Persons.js
@@ -1,23 +1,29 @@
-import React ,{Component}from 'react';
+import React ,{PureComponent}from 'react';
 import Person from "./Person/Person";
-class Persons extends Component{
+class Persons extends PureComponent{
     // static getDerivedStateFromProps(props,state){
     //     console.log('[Persons.js] getDerivedStateFromProps)')
     //     return state
     // }
-    shouldComponentUpdate(nextProps,nextState){
-        console.log('[Persons.js] shouldComponentUpdate')
-        // if(nextProps.persons !==this.props.persons){
-        //     return true
-        // }else{
-        //     return false
-        // }
-        return true
-    }
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log('[Persons.js] getSnapshotBeforeUpdate')
-        return {message:'Snapshot'}
-    }
+
+    // getSnapshotBeforeUpdate(prevProps, prevState) {
+    //     console.log('[Persons.js] getSnapshotBeforeUpdate')
+    //     return {message:'Snapshot'}
+    // }
+
+    // shouldComponentUpdate(nextProps,nextState){
+    //     console.log('[Persons.js] shouldComponentUpdate')
+    //     if( nextProps.persons !==this.props.persons ||
+    //         nextProps.changed !==this.props.changed ||
+    //         nextProps.clicked !==this.props.clicked
+    //     ){
+    //         return true
+    //     }else{
+    //         return false
+    //     }
+    //     // return true
+    // }
+
 
     render(){
         console.log('[Persons.js] rendering')
