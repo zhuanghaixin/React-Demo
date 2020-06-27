12dc6ebde9ca01c006c1842af5c4c77750b4e28c
zhuanghaixin <649308293@qq.com>
2020-06-27 12:07:29 +0800

    feat: React基础项目 控制汉堡🍔的材料 添加或删除

--- my-burger-builder/src/components/Burger/Burger.js
+++ my-burger-builder/src/components/Burger/Burger.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Burger.css'
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 const burger=(props)=>{
-    const transformIngredients=Object.keys(props.ingredients).map((igKey)=>{
+    let transformIngredients=Object.keys(props.ingredients).map((igKey)=>{
         return [...Array(props.ingredients[igKey])].map((_,i)=>{
             console.log('i')
             console.log(i)
@@ -11,9 +11,16 @@ const burger=(props)=>{
                 key={igKey+i}
             ></BurgerIngredient>
         })
-    })
+        //平铺数组
+    }).reduce((arr,el)=>{
+        return arr.concat(el)
+    },[])
+
     console.log('transformIngredients')
     console.log(transformIngredients)
+    if(transformIngredients.length===0){
+        transformIngredients=<p>Please start adding ingredients!</p>
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"></BurgerIngredient>
